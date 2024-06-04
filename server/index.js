02448c3bafbd62954109261ccee4d073943a6705
mohaimin37 <137748062+mohaimin37@users.noreply.github.com>
2024-06-04 21:04:36 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require("cors")
+require("../database/mongoose.js")
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(require("./login.js"))
+app.use(require("./cartpro.js"))
+app.use(require("./prodetails.js"))
+app.use(cors())
+
+
+
+
+const PORT = process.env.PORT || 8000
+
+
+app.listen(PORT,()=>{
+
+    console.log("hi how are you are you fine" )
+})
+
+
+
